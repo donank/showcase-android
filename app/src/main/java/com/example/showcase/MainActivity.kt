28f4487c1bfb7bd3fdaaa7e839db6f0eea03242d
DonAnk <11donank22@gmail.com>
2020-09-07 23:36:09 +0530

    added MainActivity lifecycle elements

--- app/src/main/java/com/example/showcase/MainActivity.kt
+++ app/src/main/java/com/example/showcase/MainActivity.kt
@@ -4,8 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart(){
+        super.onStart()
+    }
+
+    override fun onStop(){
+        super.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
