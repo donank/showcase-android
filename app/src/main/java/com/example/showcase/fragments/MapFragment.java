a15477affc1f0dd5bb60990aaf0509f52ef94ecf
DonAnk <11donank22@gmail.com>
2020-09-10 22:14:25 +0530

    added arcGIS map view fragment

---
+++ app/src/main/java/com/example/showcase/fragments/MapFragment.java
@@ -0,0 +1,69 @@
+package com.example.showcase.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.example.showcase.R;
+
+public class MapFragment extends Fragment {
+
+    private MapView mapView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = view.findViewById(R.id.map_view);
+        setupMap();
+    }
+
+    private void setupMap() {
+        if(mapView != null){
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double lat = 34.0720;
+            double lon = -118.8050;
+            int levelOfDetail = 13;
+            ArcGISMap map = new ArcGISMap(basemapType, lat, lon, levelOfDetail);
+            mapView.setMap(map);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mapView != null) {
+            mapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        if (mapView != null) {
+            mapView.resume();
+        }
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mapView != null) {
+            mapView.dispose();
+        }
+        super.onDestroy();
+    }
+}
